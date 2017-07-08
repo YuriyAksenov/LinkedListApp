0000aacf6baea911da781644617d267adf23751f
Yuriy Aksenov <Yura290610@yandex.ru>
2017-07-08 23:58:06 +0300

    Доделан LinkedList со событиями. Сделаны тесты для методом и событий

---
+++ LinkedList.Test/LinkedListTest.cs
@@ -0,0 +1,247 @@
+﻿using NUnit.Framework;
+
+namespace LinkedList.Test
+{
+    [TestFixture]
+    public class LinkedListTest
+    {
+        private LinkedList<int> _linkedList;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _linkedList = new LinkedList<int>();
+        }
+
+        [Test]
+        public void CountTest()
+        {
+            Assert.AreEqual(0, _linkedList.Count);
+
+            for (int i = 0; i < 5; i++)
+            {
+                _linkedList.Add(i);
+            }
+
+            Assert.AreEqual(5, _linkedList.Count);
+        }
+
+        [Test]
+        public void First()
+        {
+            Assert.AreEqual(0, _linkedList.Count);
+            Assert.AreEqual(null, _linkedList.First);
+            _linkedList.Add(5);
+            _linkedList.Add(6);
+            _linkedList.Add(7);
+            Assert.AreEqual(5, _linkedList.First.Value);
+        }
+
+        [Test]
+        public void Last()
+        {
+            Assert.AreEqual(0, _linkedList.Count);
+            Assert.AreEqual(null, _linkedList.Last);
+            _linkedList.Add(5);
+            _linkedList.Add(6);
+            _linkedList.Add(7);
+            Assert.AreEqual(7, _linkedList.Last.Value);
+        }
+
+        [Test]
+        public void IsReadOnlyTest()
+        {
+            Assert.AreEqual(_linkedList.IsReadOnly, false);
+        }
+
+        [Test]
+        public void AddTest()
+        {
+            Assert.AreEqual(0, _linkedList.Count);
+            for (int i = 0; i < 5; i++)
+            {
+               _linkedList.Add(i+2);
+            }
+
+            Assert.AreEqual(5, _linkedList.Count);
+
+            LinkedListNode<int> tempNode = _linkedList.First;
+
+            for (int i = 0; i < 5; i++)
+            {
+                Assert.AreEqual(i+2, tempNode.Value);
+                tempNode = tempNode.Next;
+            }
+
+        }
+
+        [Test]
+        public void ClearTest()
+        {
+
+            Assert.AreEqual(0, _linkedList.Count);
+
+            for (int i = 0; i < 5; i++)
+            {
+               _linkedList.Add(1);
+            }
+
+            Assert.AreEqual( 5, _linkedList.Count);
+
+            _linkedList.Clear();
+
+           Assert.AreEqual(0, _linkedList.Count);
+
+        }
+
+        [Test]
+        public void ContainsTest()
+        {
+            Assert.AreEqual(false, _linkedList.Contains(10));
+            _linkedList.Add(10);
+            Assert.AreEqual(true, _linkedList.Contains(10));
+
+            for (int i = 0; i < 5; i++)
+            {
+                _linkedList.Add(i);
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                Assert.AreEqual(true, _linkedList.Contains(i));
+            }
+        }
+
+        [Test]
+        public void CopyToTest()
+        {
+            int[] array = new int[10];
+            for (int i = 0; i < 2; i++)
+            {
+                array[i] = i;
+            }
+
+            for (int i = 2; i < 10; i++)
+            {
+                _linkedList.Add(i);
+            }
+
+            _linkedList.CopyTo(array, 2);
+
+            for (int i = 0; i < 10; i++)
+            {
+                Assert.AreEqual(i,array.GetValue(i));
+            }
+        }
+
+        [Test]
+        public void RemoveTest()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                _linkedList.Add(i);
+            }
+
+            Assert.AreEqual(10, _linkedList.Count);
+
+            for (int i = 0; i < 10; i++)
+            {
+                Assert.AreEqual(true,_linkedList.Contains(i));
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+               Assert.AreEqual(true,_linkedList.Remove(i));
+            }
+
+            Assert.AreEqual(0, _linkedList.Count);
+
+            Assert.AreEqual(false, _linkedList.Remove(20));
+
+        }
+
+        [Test]
+        public void GetEnumeratorTest()
+        {
+            string s = "";
+            string trueS = "";
+            for (int i = 0; i < 10; i++)
+            {
+                _linkedList.Add(i);
+                s += i.ToString();
+            }
+
+            foreach (var item in _linkedList)
+            {
+                trueS += item.ToString();
+            }
+
+            Assert.AreEqual(trueS,s);
+        }
+
+        [Test]
+        public void InsertTest()
+        {
+            Assert.AreEqual(0, _linkedList.Count);
+            string s = "";
+            string trueS = "";
+            for (int i = 1; i < 6; i++)
+            {
+                _linkedList.Add(i);
+                trueS +=(i*10).ToString()+" "+ i.ToString()+" ";
+            }
+
+            for (int i = 1; i < 6; i++)
+            {
+                _linkedList.Insert(i,i*10);
+            }
+
+            LinkedListNode<int> tempNode = _linkedList.First;
+
+            for (int i = 0; i < 10; i++)
+            {
+                s+=tempNode.Value+" ";
+                tempNode = tempNode.Next;
+            }
+
+            Assert.AreEqual(trueS,s);
+            Assert.AreEqual(10, _linkedList.Count);
+        }
+
+        
+
+        [Test]
+        public void AddedTest()
+        {
+            _linkedList.Added += delegate (object sender, LinkedListEventArgs e)
+            {
+                Assert.Pass();
+            };
+
+            _linkedList.Add(5);
+        }
+
+        [Test]
+        public void ClearedTest()
+        {
+            _linkedList.Cleared += delegate (object sender, LinkedListEventArgs e)
+            {
+                Assert.Pass();
+            };
+
+            _linkedList.Clear();
+        }
+
+        [Test]
+        public void RemovedTest()
+        {
+            _linkedList.Removed += delegate (object sender, LinkedListEventArgs e)
+            {
+                Assert.Pass();
+            };
+
+            _linkedList.Remove(5);
+        }
+
+    }
+}
--- LinkedList/LinkedList.cs
+++ LinkedList/LinkedList.cs
@@ -1,152 +1,248 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace LinkedList
 {
-    public class LinkedList<T> : IEnumerable<T>, ICollection<T>
+    public class LinkedListEventArgs : EventArgs
     {
-        private class Node
+        public bool IsExecuted { get; }
+        public string Message { get; }
+
+        public LinkedListEventArgs(bool isExecuted, string message)
         {
-            public T Item { get; set; }
-            public Node Next { get; set; }
-            public Node Previous { get; set; }
+            IsExecuted = isExecuted;
+            Message = message;
+        }
+    }
+
+
+    public sealed class LinkedListNode<T>
+    {
+        public LinkedListNode(T value) : this(value, null, null, null) { }
+        internal LinkedListNode(T value, LinkedListNode<T> next, LinkedListNode<T> previous, LinkedList<T> list)
+        {
+            this.Value = value;
+            this.List = list;
+            this.Next = next;
+            this.Previous = previous;
+        }
+        public LinkedList<T> List { get; internal set; }
+        public T Value { get; set; }
+        public LinkedListNode<T> Next { get; internal set; }
+        public LinkedListNode<T> Previous { get; internal set; }
+    }
+
+
+
+    public sealed class LinkedList<T> : IEnumerable<T>, ICollection<T>
+    {
+        public LinkedList()
+        {
+            this.Count = 0;
+            this.First = null;
+            this.Last = null;
         }
 
-        private Node first;
-        private Node last;
-
-        public int Count
-        {
-            get
-            {
-                if (ReferenceEquals(first, null)) return 0;
-
-                Node iNode = first;
-                int count = 1;
-                while (!ReferenceEquals(iNode.Next, null))
-                {
-                    count++;
-                    iNode = iNode.Next;
-                }
-                return count;
-            }
-        }
-
+        public int Count { get; private set; }
+        public LinkedListNode<T> First { get; private set; }
+        public LinkedListNode<T> Last { get; private set; }
         public bool IsReadOnly => false;
 
-        public void Add(T item)
+        public event EventHandler<LinkedListEventArgs> Added;
+        public event EventHandler<LinkedListEventArgs> Removed;
+        public event EventHandler<LinkedListEventArgs> Cleared;
+
+        /*
+        //public int Count
+        //{
+        //    get
+        //    {
+        //        if (ReferenceEquals(null,first)) return 0;
+
+        //        Node iNode = first;
+        //        int count = 1;
+        //        while (!ReferenceEquals(iNode, last))
+        //        {
+        //            count++;
+        //            iNode = iNode.Next;
+        //        }
+        //        return count;
+        //    }
+        //}
+
+        //public T First()
+        //{
+        //    if (ReferenceEquals(first, null)) throw new InvalidOperationException("LinkedList is empty");
+
+        //    if(ReferenceEquals(first,last))
+        //    {
+        //        Node returnedNode = first;
+        //        first = null;
+        //        last = null;
+        //        return returnedNode.Value;
+        //    }
+
+        //    Node returnedFirst = first;
+        //    first.Next.Previous = null;
+        //    first = first.Next;
+
+        //    return returnedFirst.Value;   
+        //}
+
+        //public T Last()
+        //{
+        //    if (ReferenceEquals(last, null)) throw new InvalidOperationException("LinkedList is empty");
+        //    if (ReferenceEquals(first, last))
+        //    {
+        //        Node returnedNode = first;
+        //        first = null;
+        //        last = null;
+        //        return returnedNode.Value;
+        //    }
+
+        //    Node returnedLast = last;
+        //    last.Previous.Next = null;
+        //    last = last.Previous;
+
+        //    return returnedLast.Value;
+        //}
+        */
+
+        public void Add(T value)
         {
-            //if (ReferenceEquals(item, null)) throw new ArgumentNullException("item", "A null reference is passed to a method.");
-            Node newNode = new Node();
+            LinkedListNode<T> newNode = new LinkedListNode<T>(value, null, Last, this);
 
-            if (ReferenceEquals(first, null))
+
+            if (ReferenceEquals(null, First))
             {
-
-                newNode.Item = item;
-                newNode.Next = null;
-                newNode.Previous = null;
-                first = newNode;
-                last = newNode;
+                First = newNode;
+                Last = newNode;
+                Count = 1;
+                Added?.Invoke(this, new LinkedListEventArgs(true, "The value was added into the LinkedList."));
                 return;
             }
-
-            newNode.Item = item;
-            newNode.Next = null;
-            newNode.Previous = last;
-            last.Next = newNode;
-            last = newNode;
-            return;        
+            Last.Next = newNode;
+            Last = newNode;
+            Count++;
+            Added?.Invoke(this, new LinkedListEventArgs(true, "The value was added into the LinkedList."));
+            return;
         }
 
         public void Clear()
         {
-            if (ReferenceEquals(first, null)) return;
-            if (ReferenceEquals(first.Next, null))
+            if (ReferenceEquals(null, First))
             {
-                first = null;
-                last = null;
+                Cleared?.Invoke(this, new LinkedListEventArgs(true, "The LinkedList has been already cleared."));
                 return;
             }
 
-            Node deletedNode = first.Next;
-            while (!ReferenceEquals(deletedNode.Next, last))
-            {
+            Count = 0;
 
-                deletedNode.Previous = null;
+            if (ReferenceEquals(First, Last))
+            {
+                First = null;
+                Last = null;
+                Cleared?.Invoke(this, new LinkedListEventArgs(true, "The LinkedList was cleared."));
+                return;
+            }
+
+            LinkedListNode<T> deletedNode = First.Next;
+
+            while (!ReferenceEquals(deletedNode, Last))
+            {
                 deletedNode.Previous.Next = null;
+                deletedNode.Previous = null;
                 deletedNode = deletedNode.Next;
             }
-            last.Previous = null;
-            last = null;
+            First.Next = null;
+            First = null;
+            Last.Previous = null;
+            Last = null;
+            Cleared?.Invoke(this, new LinkedListEventArgs(true, "The LinkedList was cleared."));
             return;
         }
 
-        public bool Contains(T item)
+        public bool Contains(T value)
         {
-            //if (ReferenceEquals(item, null)) throw new ArgumentNullException("item", "A null reference is passed to a method.");
-            if (ReferenceEquals(first, null)) return false;
-
-            Node iNode = first;
-            while (!ReferenceEquals(iNode.Next, null))
+            //if (ReferenceEquals(value, null)) throw new ArgumentNullException("value", "A null reference is passed to a method.");
+            if (ReferenceEquals(First, null)) return false;
+            if (ReferenceEquals(First, Last))
             {
-                if (ReferenceEquals(iNode.Item, item)) return true;
-                if (iNode.Item.GetType() != item.GetType())
+
+                if (ReferenceEquals(First.Value, value)) return true;
+                if (First.Value.GetType() != value.GetType()) return false;
+                if (First.Value.Equals(value)) return true;
+                return false;
+            }
+
+            LinkedListNode<T> iNode = First;
+            while (!ReferenceEquals(iNode, Last))
+            {
+                if (ReferenceEquals(iNode.Value, value)) return true;
+                if (iNode.Value.GetType() != value.GetType())
                 {
                     iNode = iNode.Next;
                     continue;
                 }
-                if (iNode.Equals(item)) return true;
+                if (iNode.Value.Equals(value)) return true;
                 iNode = iNode.Next;
             }
+
+            if (ReferenceEquals(Last.Value, value)) return true;
+            if (iNode.Value.GetType() != value.GetType()) return false;
+            if (iNode.Value.Equals(value)) return true;
             return false;
         }
 
-        public void CopyTo(T[] array, int arrayIndex)
+        public void CopyTo(T[] array, int index)
         {
             if (ReferenceEquals(array, null)) throw new ArgumentNullException("array", "A null reference is passed to a method.");
-            if (arrayIndex < 0) throw new ArgumentOutOfRangeException();
-            if (array.Rank > 1 || (this.Count > array.Length - arrayIndex)) throw new ArgumentException();
+            if (index < 0) throw new ArgumentOutOfRangeException();
+            if (array.Rank > 1 || (this.Count > array.Length - index)) throw new ArgumentException();
 
-            Node iNode = first;
-            while (!ReferenceEquals(iNode.Next, null))
+            if (ReferenceEquals(First, null)) return;
+            if (ReferenceEquals(First, Last))
             {
-                array[arrayIndex] = iNode.Item;
-                arrayIndex++;
+                array[index] = First.Value;
+            }
+
+            LinkedListNode<T> iNode = First;
+            while (!ReferenceEquals(iNode, Last))
+            {
+                array[index] = iNode.Value;
+                index++;
                 iNode = iNode.Next;
             }
 
-            //ArrayTypeMismatchException
-            //Тип исходного массива Array не может быть автоматически приведен к типу массива назначения array.
-            //RankException
-            //Исходный массив — многомерный.
-            //InvalidCastException
-            //По меньшей мере один элемент в исходном массиве Array нельзя привести к типу массива назначения array.
+            array[index] = iNode.Value;
         }
 
-        public bool Remove(T item)
+        public bool Remove(T value)
         {
-            if (ReferenceEquals(first, null)) return false;
+            if (ReferenceEquals(null, First))
+            {
+                Removed?.Invoke(this, new LinkedListEventArgs(false, "The LinkedList is empty. Removing is canceled."));
+                return false;
+            }
 
             bool isContain = false;
 
-            Node iNode = first;
-            while (!ReferenceEquals(iNode.Next, null) || ReferenceEquals(iNode, first))
+            LinkedListNode<T> iNode = First;
+            while (!ReferenceEquals(iNode, Last))
             {
-                if (ReferenceEquals(iNode.Item, item))
+                if (ReferenceEquals(iNode.Value, value))
                 {
                     isContain = true;
                     break;
                 }
-                if (iNode.Item.GetType() != item.GetType())
+                if (iNode.Value.GetType() != value.GetType())
                 {
                     iNode = iNode.Next;
                     continue;
                 }
-                if (iNode.Equals(item))
+                if (iNode.Value.Equals(value))
                 {
                     isContain = true;
                     break;
@@ -154,24 +250,29 @@ namespace LinkedList
                 iNode = iNode.Next;
             }
 
+            if (ReferenceEquals(Last.Value, value) && !isContain) isContain = true;
+            if (iNode.Value.GetType() != value.GetType() && !isContain) isContain = false;
+            if (iNode.Value.Equals(value) && !isContain) isContain = true;
+
             if (isContain)
             {
-                if (ReferenceEquals(iNode, first) && ReferenceEquals(iNode, last))
+                Count--;
+                if (ReferenceEquals(iNode, First) && ReferenceEquals(iNode, Last))
                 {
-                    first = null;
-                    last = null;
+                    First = null;
+                    Last = null;
                     return true;
                 }
-                if (ReferenceEquals(iNode, first))
+                if (ReferenceEquals(iNode, First))
                 {
-                    first.Next.Previous = null;
-                    first = first.Next;
+                    First.Next.Previous = null;
+                    First = First.Next;
                     return true;
                 }
-                if (ReferenceEquals(iNode, last))
+                if (ReferenceEquals(iNode, Last))
                 {
-                    last.Previous.Next = null;
-                    last  =last.Previous;
+                    Last.Previous.Next = null;
+                    Last = Last.Previous;
                     return true;
                 }
 
@@ -180,24 +281,25 @@ namespace LinkedList
                 iNode.Previous = null;
                 iNode.Next = null;
                 iNode = null;
+                Removed?.Invoke(this, new LinkedListEventArgs(true, "The passing element was removed."));
                 return true;
             }
 
+            Removed?.Invoke(this, new LinkedListEventArgs(false, "The passing element did not find. Removing is canceled."));
             return false;
         }
 
         public IEnumerator<T> GetEnumerator()
         {
-            if (ReferenceEquals(first, null)) yield break;
+            if (ReferenceEquals(First, null)) yield break;
 
-            yield return first.Item;
-
-            Node iNode = first;
-            while (!ReferenceEquals(iNode.Next, null))
+            LinkedListNode<T> iNode = First;
+            while (!ReferenceEquals(iNode, Last))
             {
-                yield return iNode.Item;
+                yield return iNode.Value;
                 iNode = iNode.Next;
             }
+            yield return Last.Value;
         }
 
         IEnumerator IEnumerable.GetEnumerator()
@@ -205,46 +307,51 @@ namespace LinkedList
             return GetEnumerator();
         }
 
-        public void Insert(T item, T insertedItem)
+        public void Insert(T beforeValue, T value)
         {
+            if (ReferenceEquals(First, null)) return;
 
-        }
+            bool isContain = false;
 
-        public T First()
-        {
-            if (ReferenceEquals(first, null)) throw new InvalidOperationException("LinkedList is empty");
-
-            if(ReferenceEquals(first,last))
+            LinkedListNode<T> iNode = First;
+            while (!ReferenceEquals(iNode, Last))
             {
-                Node returnedNode = first;
-                first = null;
-                last = null;
-                return returnedNode.Item;
+                if (ReferenceEquals(iNode.Value, beforeValue))
+                {
+                    isContain = true;
+                    break;
+                }
+                if (iNode.Value.GetType() != beforeValue.GetType())
+                {
+                    iNode = iNode.Next;
+                    continue;
+                }
+                if (iNode.Value.Equals(beforeValue))
+                {
+                    isContain = true;
+                    break;
+                }
+                iNode = iNode.Next;
             }
 
-            Node returnedFirst = first;
-            first.Next.Previous = null;
-            first = first.Next;
+            if (ReferenceEquals(Last.Value, beforeValue) && !isContain) isContain = true;
+            if (iNode.Value.GetType() != beforeValue.GetType() && !isContain) isContain = false;
+            if (iNode.Value.Equals(beforeValue) && !isContain) isContain = true;
 
-            return returnedFirst.Item;   
-        }
-
-        public T Last()
-        {
-            if (ReferenceEquals(last, null)) throw new InvalidOperationException("LinkedList is empty");
-            if (ReferenceEquals(first, last))
+            if (isContain)
             {
-                Node returnedNode = first;
-                first = null;
-                last = null;
-                return returnedNode.Item;
+                LinkedListNode<T> newNode = new LinkedListNode<T>(value, iNode, iNode.Previous, this);
+                if (ReferenceEquals(First, iNode))
+                {
+                    First = newNode;
+                    Count++;
+                    return;
+                }
+                iNode.Previous.Next = newNode;
+                iNode.Previous = newNode;
+                Count++;
+                return;
             }
-
-            Node returnedLast = last;
-            last.Previous.Next = null;
-            last = last.Previous;
-
-            return returnedLast.Item;
         }
     }
 }
